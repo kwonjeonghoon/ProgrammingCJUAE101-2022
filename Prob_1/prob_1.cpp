5bbcad338ee39bd8f7a772241cbd96d24eb3b3ab
kwon jeonghun <lgf240@naver.com>
2022-09-21 11:43:20 +0900

    prob_1
    주교재, pp.50,확인문제 3번입니다.

---
+++ Prob_1/prob_1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	char ch1 = 'A';
+	char ch2 = 65;
+	char ch3 = 't';
+	char ch4 = 116;
+
+	printf("Enter characters : %c\n", ch1, ch1);
+	printf("The ASCII code value of the %c character is %d.\n", ch2, ch2);
+	printf("Enter characters : %c\n", ch3, ch3);
+	printf("The ASCII code value of the %c character is %d.\n", ch4, ch4);
+
+	return 0;
+}
